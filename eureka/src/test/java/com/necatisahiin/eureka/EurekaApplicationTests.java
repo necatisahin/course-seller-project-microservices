5a838e5ab82413203e9d0a68d6655fa588700e1b
necatisahin <necatisahiin@gmail.com>
2022-08-21 03:22:55 +0300

    Add files via upload

---
+++ eureka/src/test/java/com/necatisahiin/eureka/EurekaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.necatisahiin.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
